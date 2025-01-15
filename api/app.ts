3241ef285f3f6d2e5eb86f316bc12e517ab3d29c
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-15 15:36:00 -0300

    Update app.ts

--- api/app.ts
+++ api/app.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
 import { cors } from "hono/cors";
+import { serveStatic } from "hono/bun";
+import { authRoute } from "./routes/auth";
 
 const app = new Hono();
 
@@ -14,6 +16,13 @@ app.use(
   })
 );
 
-app.route("/expenses", expensesRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", expensesRoute)
+  .route("/", authRoute);
+
+app.get("*", serveStatic({ root: "../web/dist" }));
+app.get("*", serveStatic({ path: "../web/dist/index.html" }));
 
 export default app;
+export type ApiRoutes = typeof apiRoutes;
