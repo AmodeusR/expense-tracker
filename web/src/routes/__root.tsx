d2de4f4d2a7a92a7099b1b8d264e64070bde8cc7
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-15 15:31:15 -0300

    Create root page route

---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: () => {
+    const linkStyles = cn("[&.active]:bg-zinc-800 font-medium px-6 py-2 hover:bg-zinc-900 transition-colors");
+
+    return (
+    <>
+      <nav className="flex items-center mx-10">
+        <Link to="/" className={linkStyles}>
+          Home
+        </Link>{" "}
+        <Link to="/about" className={linkStyles}>
+          About
+        </Link>
+        <Link to="/expenses" className={linkStyles}>
+          Expenses
+        </Link>
+        <Link to="/create-expense" className={linkStyles}>
+          Create Expense
+        </Link>
+      </nav>
+      <hr className="border-zinc-800" />
+      <Outlet />
+    </>
+  )},
+});
