53cdb6fb0426c8e7823e8bc94aa5bbad4d0afd98
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-15 15:30:08 -0300

    Create app's main page

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { getTotalSpent } from "@/services/expenses";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+
+
+function Index() {
+  const { isPending, data, error } = useQuery({
+    queryKey: ["get-total-spent"],
+    queryFn: getTotalSpent,
+  });
+
+  return (
+    <div className="bg-background m-10 flex flex-col items-center">
+      <Card className="w-[300px] p-2">
+        <CardHeader>
+          <CardTitle>Teste</CardTitle>
+          <CardDescription>Total amount spent</CardDescription>
+        </CardHeader>
+        <CardContent>
+          {error
+            ? "Something went wrong"
+            : isPending
+              ? "..."
+              : data?.totalAmount}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
