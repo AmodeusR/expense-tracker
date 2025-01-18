43a25fc2305d1bc18fa22e4e2a6b1c6ffcc04fd6
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-18 17:56:42 -0300

    Fix kindeClient with correct client secret

--- api/kinde.ts
+++ api/kinde.ts
@@ -15,7 +15,7 @@ export const kindeClient = createKindeServerClient(
   {
     authDomain: process.env.KINDE_AUTH_DOMAIN!,
     clientId: process.env.KINDE_CLIENT_ID!,
-    clientSecret: process.env.KINDE_CLIENT_ID!,
+    clientSecret: process.env.KINDE_CLIENT_SECRET!,
     redirectURL: process.env.KINDE_REDIRECT_URL!,
     logoutRedirectURL: process.env.KINDE_LOGOUT_REDIRECT_URL!,
   }
