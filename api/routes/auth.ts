f11d3d3a901ed525d9ae4d1e717e1009d0ad09d6
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-18 21:22:08 -0300

    Update auth routes to proper formatting
    Hono routes typescript only works if routes are chained from hono instance.

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -1,34 +1,29 @@
 import { Hono } from "hono";
 import { getUser, honoSessionManager, kindeClient } from "../kinde";
 
-export const authRoute = new Hono();
+export const authRoute = new Hono()
+  .get("/login", async (c) => {
+    const loginUrl = await kindeClient.login(honoSessionManager(c));
 
-authRoute.get("/login", async (c) => {
-  const loginUrl = await kindeClient.login(honoSessionManager(c));
+    return c.redirect(loginUrl.toString());
+  })
+  .get("/register", async (c) => {
+    const registerUrl = await kindeClient.register(honoSessionManager(c));
 
-  return c.redirect(loginUrl.toString());
-});
+    return c.redirect(registerUrl.toString());
+  })
+  .get("/callback", async (c) => {
+    const url = new URL(c.req.url);
+    await kindeClient.handleRedirectToApp(honoSessionManager(c), url);
+    return c.redirect("/");
+  })
+  .get("/logout", async (c) => {
+    const logoutUrl = await kindeClient.logout(honoSessionManager(c));
 
-authRoute.get("/register", async (c) => {
-  const registerUrl = await kindeClient.register(honoSessionManager(c));
+    return c.redirect(logoutUrl.toString());
+  })
+  .get("/me", getUser, async (c) => {
+    const user = c.var.user;
 
-  return c.redirect(registerUrl.toString());
-});
-
-authRoute.get("/callback", async (c) => {
-  const url = new URL(c.req.url);
-  await kindeClient.handleRedirectToApp(honoSessionManager(c), url);
-  return c.redirect("/");
-});
-
-authRoute.get("/logout", async (c) => {
-  const logoutUrl = await kindeClient.logout(honoSessionManager(c));
-
-  return c.redirect(logoutUrl.toString());
-});
-
-authRoute.get("/me", getUser, async (c) => {
-  const user = c.var.user;
-
-  return c.json({ user });
-})
+    return c.json({ user });
+  });
