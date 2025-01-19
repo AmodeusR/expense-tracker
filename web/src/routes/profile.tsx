47a7d0c662bebbba5e4e065e6edff06faf331776
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-18 21:22:44 -0300

    Configure getCurrentUser useQuery fn

--- web/src/routes/profile.tsx
+++ web/src/routes/profile.tsx
@@ -1,4 +1,6 @@
 import { api } from "@/lib/api";
+import { getCurrentUser } from "@/services/auth";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
 
@@ -7,7 +9,8 @@ export const Route = createFileRoute("/profile")({
 })
 
 function Profile() {
-  const userProfile = api
+  const { data, isLoading, error} = useQuery({ queryKey: ["get-user"], queryFn: getCurrentUser})
+
   return (
     <div className="flex flex-col items-center mt-10 gap-4">Profile</div>
   )
