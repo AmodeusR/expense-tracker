14d5121b28f0f5235703d2e4efb045f9d8970a89
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-26 18:48:51 -0300

    Create User type

--- web/src/lib/api.types.ts
+++ web/src/lib/api.types.ts
@@ -1,7 +1,12 @@
+import { getCurrentUser } from "./../services/auth";
 import { ClientResponse } from "hono/client";
 import { api } from "./api";
 
 type ExtractResponseData<T> = T extends ClientResponse<infer U> ? U : never;
-type ExtractExpenses<T> = T extends { expenses: (infer U)[]} ? U : never;
+type ExtractExpenses<T> = T extends { expenses: (infer U)[] } ? U : never;
 
-export type Expense =  ExtractExpenses<ExtractResponseData<Awaited<ReturnType<typeof api.expenses.$get>>>>;
+export type Expense = ExtractExpenses<
+  ExtractResponseData<Awaited<ReturnType<typeof api.expenses.$get>>>
+>;
+
+export type User = Awaited<ReturnType<typeof getCurrentUser>>["user"];
