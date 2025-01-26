fcef29a8726110f4f1bb039ea8a83c209703db1b
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-26 18:48:33 -0300

    Allow the use of queryClient out of react context

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,17 +5,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen.ts";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-const router = createRouter({ routeTree});
+const queryClient = new QueryClient();
+
+const router = createRouter({ routeTree, context: { queryClient } });
 
 declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
-
-
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
