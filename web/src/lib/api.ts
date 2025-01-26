0fec5728c89c275e6f3a8a9596484ebbc7ff4b9a
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-01-26 20:11:23 -0300

    Create constant api paths for login/register

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -4,3 +4,6 @@ import { hc } from "hono/client";
 const client = hc<ApiRoutes>("/");
 
 export const api = client.api;
+
+export const loginPath = "https://expense-tracker-ug6r.onrender.com/api/login";
+export const registerPath = "https://expense-tracker-ug6r.onrender.com/api/register";
