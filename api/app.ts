b7cc5aff0dc15f8bea22b9d2c4dd18f76e23a421
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-05 21:35:17 -0300

    Remove static serve

--- api/app.ts
+++ api/app.ts
@@ -2,7 +2,6 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
 import { cors } from "hono/cors";
-import { serveStatic } from "hono/bun";
 import { authRoute } from "./routes/auth";
 
 const app = new Hono();
@@ -21,9 +20,6 @@ const apiRoutes = app
   .basePath("/api")
   .route("/expenses", expensesRoute)
   .route("/", authRoute);
-
-app.get("*", serveStatic({ root: "../web/dist" }));
-app.get("*", serveStatic({ path: "../web/dist/index.html" }));
 
 export default app;
 export type ApiRoutes = typeof apiRoutes;
