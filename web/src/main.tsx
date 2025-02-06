30b2f3331025cc466556eeb9ee4077d720a39c9b
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-05 21:42:11 -0300

    move css import to top of file

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen.ts";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
