64c76e8efafaaa20940b1043df9e5420e1fe5b9a
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-06 23:58:26 -0300

    Fix vite env import

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,7 +2,7 @@ import { type ApiRoutes } from "@api/app";
 import { hc } from "hono/client";
 
 
-const client = hc<ApiRoutes>(import.meta.env.API_URL, {init: {
+const client = hc<ApiRoutes>(import.meta.env.VITE_API_URL, {init: {
   credentials: "include"
 }});
 
