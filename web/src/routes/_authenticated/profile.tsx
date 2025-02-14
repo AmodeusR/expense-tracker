e41a408eed174bf9bb2976035642cd0e6d74ce03
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-14 17:27:28 -0300

    Add profile image to Profile page

--- web/src/routes/_authenticated/profile.tsx
+++ web/src/routes/_authenticated/profile.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { api } from "@/lib/api";
 import { userQueryOptions } from "@/lib/api.queries";
 import { useQuery } from "@tanstack/react-query";
@@ -9,6 +10,7 @@ export const Route = createFileRoute("/_authenticated/profile")({
 
 function Profile() {
   const { data, isLoading, error } = useQuery(userQueryOptions);
+  const userPicture = data?.user.picture === null ? undefined : data?.user.picture;
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -18,9 +20,15 @@ function Profile() {
     console.error(error);
     return <p>Error: {error.message}</p>;
   }
+
   return (
     <div className="flex flex-col items-center gap-4">
-      <h1>Profile</h1>
+      <Avatar className="w-24 h-24">
+        <AvatarImage src={userPicture} alt="User's profile picture" />
+        <AvatarFallback className="text-sm font-semibold">User</AvatarFallback>
+      </Avatar>
+
+      <h2 className="text-2xl font-semibold">Profile</h2>
       <p>Welcome, {data?.user.given_name}</p>
       <a
         href={api.logout.$url().href}
