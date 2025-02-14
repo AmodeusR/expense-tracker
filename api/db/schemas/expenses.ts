4312a085e1c14cffa9d8fb155962ed02ef2ff213
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-14 16:53:04 -0300

    update schema w/ createdAt

--- api/db/schemas/expenses.ts
+++ api/db/schemas/expenses.ts
@@ -1,13 +1,37 @@
-import { index, numeric, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  index,
+  numeric,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
-export const expensesTable = pgTable("expenses", {
-  id: serial("id").primaryKey(),
-  userId: text("user_id").notNull(),
-  title: text("title").notNull(),
-  amount: numeric("amount", { precision: 12, scale: 2 }).notNull(),
-  createdAt: timestamp("created_at").notNull().defaultNow(),
-}, (expenses) => {
-  return {
-    userIdIndex: index("namde_idx").on(expenses.userId),
+export const expensesTable = pgTable(
+  "expenses",
+  {
+    id: serial("id").primaryKey(),
+    userId: text("user_id").notNull(),
+    title: text("title").notNull(),
+    amount: numeric("amount", { precision: 12, scale: 2 }).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+  },
+  (expenses) => {
+    return [
+      index("namde_idx").on(expenses.userId),
+    ];
   }
+);
+
+export const insertExpensesSchema = createInsertSchema(expensesTable, {
+  title: z.string().min(3).max(25),
+  amount: z
+    .string()
+    .regex(
+      /^\d+(\.\d{1,2})?$/,
+      "The value must be positive and decimal up to two digits"
+    ),
 });
+export const selectExpensesSchema = createSelectSchema(expensesTable);
