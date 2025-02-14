1f1806be11d13d60d61e4ff765f6ee55e714eac9
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-14 17:27:48 -0300

    Rework About interface

--- web/src/routes/about.tsx
+++ web/src/routes/about.tsx
@@ -6,6 +6,9 @@ export const Route = createFileRoute("/about")({
 
 function About() {
   return (
-    <p>This is the about page</p>
+    <div className="flex flex-col items-center gap-4">
+      <h2 className="text-2xl font-semibold">About</h2>
+      <p className="max-w-md text-center">A simple fullstack project to practice the use of many technologies, among them are Drizzle, Kinde, Tanstack, Tailwind and much more.</p>
+    </div>
   )
 }
