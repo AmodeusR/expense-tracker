e4838703a903e59438437d703a04fa8a0962ab7d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-15 10:34:30 -0300

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,16 +8,21 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@api": path.resolve(__dirname, "../api")
+      "@api": path.resolve(__dirname, "../api"),
     },
   },
-    server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:3000",
-          changeOrigin: true,
-          // rewrite: (path) => path.replace("api", "")
-        }
-      }
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        // rewrite: (path) => path.replace("api", "")
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["zod"],
+    },
+  },
 });
