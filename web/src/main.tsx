a411e7817d0576c7440e8945a9f46dca2b3a744f
Ricardo Magalhães <ricardogleiss@hotmail.com>
2025-02-16 21:29:09 -0300

    Isolate queryClient

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,10 @@
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen.ts";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
-
-const queryClient = new QueryClient();
+import { queryClient } from "./lib/queryClient.ts";
 
 const router = createRouter({ routeTree, context: { queryClient } });
 
